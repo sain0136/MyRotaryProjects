35293addbf6941e3661fc28e9fe5924aa70f7fdb
sain0136 <55252053+sain0136@users.noreply.github.com>
2023-02-06 14:44:58 -0500

    bug:ts erorr

--- Backend/app/Controllers/Http/UploadsController.ts
+++ Backend/app/Controllers/Http/UploadsController.ts
@@ -16,41 +16,41 @@ export default class UploadsController {
    * @param  {} {request
    * @param  {HttpContextContract} response}
    */
-  public async testUpload({ request, response }: HttpContextContract) {
-    const coverImage = request.file("test");
-    const cover2 = coverImage;
-    const res = {
-      s3error: "",
-      s3url: "",
-      localurl: "",
-    };
-    try {
-      if (coverImage) {
-        await coverImage.moveToDisk("./", {}, "spaces");
-        let path = coverImage.filePath;
-        if (path) {
-          res.s3url = path;
-        }
-      }
-    } catch (error) {
-      res.s3error = error.message;
-      res.s3url = error;
-    }
-    try {
-      if (cover2) {
-        await cover2.moveToDisk("./", {}, "local");
-        let path = cover2.filePath;
-        if (path) {
-          path = await Drive.getUrl(path as string);
-          path = path.replace(/C:\/Up\//, "");
-        }
-        res.localurl = Env.get("TEST_UPLOAD_URL") + path;
-      }
-    } catch (error) {
-      res.localurl = error.message;
-    }
-    return response.json(res);
-  }
+  // public async testUpload({ request, response }: HttpContextContract) {
+  //   const coverImage = request.file("test");
+  //   const cover2 = coverImage;
+  //   const res = {
+  //     s3error: "",
+  //     s3url: "",
+  //     localurl: "",
+  //   };
+  //   try {
+  //     if (coverImage) {
+  //       await coverImage.moveToDisk("./", {}, "spaces");
+  //       let path = coverImage.filePath;
+  //       if (path) {
+  //         res.s3url = path;
+  //       }
+  //     }
+  //   } catch (error) {
+  //     res.s3error = error.message;
+  //     res.s3url = error;
+  //   }
+  //   try {
+  //     if (cover2) {
+  //       await cover2.moveToDisk("./", {}, "local");
+  //       let path = cover2.filePath;
+  //       if (path) {
+  //         path = await Drive.getUrl(path as string);
+  //         path = path.replace(/C:\/Up\//, "");
+  //       }
+  //       res.localurl = Env.get("TEST_UPLOAD_URL") + path;
+  //     }
+  //   } catch (error) {
+  //     res.localurl = error.message;
+  //   }
+  //   return response.json(res);
+  // }
 
   /**
    * @desc Handles file upload and validates uploaded files, returns
