eafb1e431015d1b0f34f777f9ed3a3b924434420
sain0136 <55252053+sain0136@users.noreply.github.com>
2023-02-06 17:59:05 -0500

    Bug:typo

--- Backend/config/cors.ts
+++ Backend/config/cors.ts
@@ -44,7 +44,7 @@ const corsConfig: CorsConfig = {
   |                     one of the above values.
   |
   */
-  origin: ['http://localhost:5173','https://backendapi.myrotaryprojects.org/'],
+  origin: ['http://localhost:5173','https://myrotaryprojects.org/'],
 
   /*
   |--------------------------------------------------------------------------
