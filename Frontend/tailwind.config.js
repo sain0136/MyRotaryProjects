8fab0c064215e59cc9a1a4ff15b45a1774f6cf70
sain0136 <55252053+sain0136@users.noreply.github.com>
2023-02-08 19:15:16 -0500

    bug: fix tailwind config

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -14,16 +14,5 @@ module.exports = {
       },
     },
   },
-  content: {
-    enabled: process.env.NODE_ENV === "production",
-    content: ["./public/**/*.html", "./src/**/*.vue"],
-    options: {
-      whitelistPatterns: [
-        /-(leave|enter|appear)(|-(to|from|active))$/,
-        /^(?!(|.*?:)cursor-move).+-move$/,
-        /^router-link(|-exact)-active$/,
-      ],
-    },
-  },
   plugins: [],
 }
