2a8fddfb6e2e195c569593fcae1e69aff98bb1fa
sain0136 <55252053+sain0136@users.noreply.github.com>
2023-02-08 19:04:57 -0500

    bug: tailwind change forconfig file

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "#ffb607",
+        "primary-black": "#212427",
+        "primary-white": "#F5F5FA",
+
+      },
+    },
+  },
+  content: {
+    enabled: process.env.NODE_ENV === "production",
+    content: ["./public/**/*.html", "./src/**/*.vue"],
+    options: {
+      whitelistPatterns: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+      ],
+    },
+  },
+  plugins: [],
+}
