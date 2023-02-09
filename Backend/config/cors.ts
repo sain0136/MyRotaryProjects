a254e87ae4c88fe1fdc1273262f81da6c277f8fa
sain0136 <55252053+sain0136@users.noreply.github.com>
2023-02-08 20:40:12 -0500

    Bug: Git issuies

--- Backend/config/cors.ts
+++ Backend/config/cors.ts
@@ -44,7 +44,7 @@ const corsConfig: CorsConfig = {
   |                     one of the above values.
   |
   */
-  origin:'*' /* ['localhost:5173','myrotaryprojects.org','myrotaryprojects.com'] */,
+  origin:['http://localhost:5173','https://myrotaryprojects.org','http://myrotaryprojects.org'],
 
   /*
   |--------------------------------------------------------------------------
