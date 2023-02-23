d089ac50f2c830b65ba11981cb71497061057984
sain0136 <55252053+sain0136@users.noreply.github.com>
2023-02-23 18:06:36 -0500

    fix: quick fix for server rollout

--- Frontend/src/modules/administration/routes/routes.ts
+++ Frontend/src/modules/administration/routes/routes.ts
@@ -6,8 +6,8 @@ import ClubAdmin from "@/modules/administration/views/ClubAdmin.vue";
 import AllProjects from "@/modules/administration/views/AllProjects.vue";
 import SiteAdminDistrictForm from "@/components/common/forms/DistrictForm.vue";
 import SiteAdminUserForm from "@/components/common/forms/UserForm.vue";
-// import SiteAdminClubForm from "@/components/common/forms/ClubForm.vue";
-// import { useRotaryStore as store } from "@/stores/rotaryStore";
+import SiteAdminClubForm from "@/components/common/forms/ClubForm.vue";
+import { useRotaryStore as store } from "@/stores/rotaryStore";
 
 const route = {
   path: "/admin",
